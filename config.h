90bd1c6af2836087428cc8920ba0ed6d234d6334
Mario Mintel <mariomintel@gmail.com>
2020-08-03 12:42:09 +0200

    update config

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 30;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -44,8 +44,8 @@ static const Layout layouts[] = {
         /* symbol     arrange function */
         { "[]=",      tile },    /* first entry is default */
         { "TTT",      bstack },
-        { "><>",      NULL },    /* no layout function means floating behavior */
         { "[M]",      monocle },
+        { "><>",      NULL },    /* no layout function means floating behavior */
 };
 
 /* key definitions */
@@ -77,16 +77,14 @@ static Key keys[] = {
         { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
         { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
         { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-        { MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
-        { MODKEY,                       XK_Tab,    view,           {0} },
-        { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
         { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
         { MODKEY,                       XK_b,      setlayout,      {.v = &layouts[1]} },
-        { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
-        { MODKEY,                       XK_space,  setlayout,      {0} },
-        { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-        { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-        { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+        { MODKEY,                       XK_space,  setlayout,      {.v = &layouts[2]} },
+        { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[3]} },
+        { MODKEY|ShiftMask,             XK_f,      togglefloating, {0} },
+        { MODKEY,                       XK_Tab,    view,           {0} },
+        { MODKEY|ShiftMask,             XK_space,  zoom,           {0} },
+        { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
         { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
         { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
         TAGKEYS(                        XK_1,                      0)
