28bd45d92e158f79777af7a131540c5a668b02d8
Mario Mintel <mariomintel@gmail.com>
2020-08-10 20:01:38 +0200

    refactor config

--- config.h
+++ config.h
@@ -1,23 +1,36 @@
 
-/* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 30;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+/* general */
+static const int nmaster           = 1;         /* number of clients in master area */
+static const int resizehints       = 0;         /* 1 means respect size hints in tiled resizals */
+static const float mfact           = 0.55;      /* factor of master area size [0.05..0.95] */
+static const unsigned int borderpx = 3;         /* border pixel of windows */
+static const unsigned int snap     = 32;        /* snap pixel */
+
+/* gaps */
+static const int smartgaps       = 0;           /* 1 means no outer gap when there is only one window */
+static const unsigned int gappih = 30;          /* horiz inner gap between windows */
+static const unsigned int gappiv = 30;          /* vert inner gap between windows */
+static const unsigned int gappoh = 20;          /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov = 30;          /* vert outer gap between windows and screen edge */
+
+/* status bar */
+static const int showbar = 1;                   /* 0 means no bar */
+static const int topbar  = 1;                   /* 0 means bottom bar */
+static const int user_bh = 30;                  /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+
+/* systray */
+static const int showsystray             = 1;   /* 0 means no systray */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 5;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 30;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+
+/* fonts */
 static const char *fonts[]          = {
         "Roboto Mono:size=10",
         "Symbols Nerd Font:size=13",
 };
+
+/* colors */
 static const char col_fg[]          = "#ebdbb2";
 static const char col_fg2[]         = "#987c76";
 static const char col_bg[]          = "#1d2021";
@@ -31,6 +44,8 @@ static const char *colors[][3]      = {
         [SchemeTaskSel]  = { col_fg,    col_bg2,   col_sel },
         [SchemeTaskHid]  = { col_fg2,   col_bg,    col_sel },
 };
+
+/* autostart */
 static const char *const autostart[] = {
         "nm-applet", NULL,
         "redshift-gtk", NULL,
@@ -42,9 +57,10 @@ static const char *const autostart[] = {
         NULL /* terminate */
 };
 
-/* tagging */
+/* tags */
 static const char *tags[] = { "", "", "", "" };
 
+/* rules */
 static const Rule rules[] = {
         /* xprop(1):
          *      WM_CLASS(STRING) = instance, class
@@ -56,11 +72,7 @@ static const Rule rules[] = {
         { "Pavucontrol",NULL,       NULL,       0,            0,           1,           -1 },
 };
 
-/* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
-
+/* layouts */
 static const Layout layouts[] = {
         /* symbol     arrange function */
         { "[]=",      tile },    /* first entry is default */
@@ -88,6 +100,10 @@ static const char *calccmd[]    = { "galculator", NULL };
 
 static Key keys[] = {
         /* modifier                     key        function        argument */
+        TAGKEYS(                        XK_1,                      0)
+        TAGKEYS(                        XK_2,                      1)
+        TAGKEYS(                        XK_3,                      2)
+        TAGKEYS(                        XK_4,                      3)
         { MODKEY,                       XK_r,      spawn,          {.v = roficmd } },
         { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
         { MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },
@@ -108,10 +124,6 @@ static Key keys[] = {
         { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
         { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
         { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-        TAGKEYS(                        XK_1,                      0)
-        TAGKEYS(                        XK_2,                      1)
-        TAGKEYS(                        XK_3,                      2)
-        TAGKEYS(                        XK_4,                      3)
         { MODKEY|ShiftMask,             XK_r,      quit,           {0} },
 };
 
